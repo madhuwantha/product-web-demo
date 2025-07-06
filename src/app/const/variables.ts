ea8e7f7d245b4ab6712e097494ba425c890186f1
madhuwantha <eng.madhuwanthapriyashan@gmail.com>
2025-07-06 12:44:08 +0530

    root

--- src/app/const/variables.ts
+++ src/app/const/variables.ts
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:3001/api";
+// export const API_URL = "http://localhost:3001/api";
+export const API_URL = "https://product-back-end-demo-9fec2d97b9b1.herokuapp.com";
