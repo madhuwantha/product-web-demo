3ae664042a41333f0af4cea493dc137b171b369a
madhuwantha <eng.madhuwanthapriyashan@gmail.com>
2025-07-06 12:46:13 +0530

    /api

--- src/app/const/variables.ts
+++ src/app/const/variables.ts
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:3001/api";
-export const API_URL = "https://product-back-end-demo-9fec2d97b9b1.herokuapp.com";
+export const API_URL = "https://product-back-end-demo-9fec2d97b9b1.herokuapp.com/api";
